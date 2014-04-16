e3d856766a15549cbafd24faf6a29aafbb6259b5
Alison Barrett <alison@barre.tt>
2014-04-15 19:21:45 -0500

    i18n: Add Grunt tasks

--- Gruntfile.js
+++ Gruntfile.js
@@ -84,6 +84,38 @@ module.exports = function( grunt ) {
 				src: ['**/*'],
 				dest: 'take_notice/'
 			}		
+		},
+		pot: {
+			options:{
+				text_domain: 'takenotice', //Your text domain. Produces my-text-domain.pot
+				dest: 'languages/', //directory to place the pot file
+				keywords: [ //WordPress localisation functions
+					'__:1',
+					'_e:1',
+					'_x:1,2c',
+					'esc_html__:1',
+					'esc_html_e:1',
+					'esc_html_x:1,2c',
+					'esc_attr__:1',
+					'esc_attr_e:1',
+					'esc_attr_x:1,2c',
+					'_ex:1,2c',
+					'_n:1,2',
+					'_nx:1,2,4c',
+					'_n_noop:1,2',
+					'_nx_noop:1,2,3c'
+				]
+			},
+			files:{
+				src:  [ '**/*.php', '!node_modules/**/*.php' ], //Parse all php files
+				expand: true
+			}
+		},
+		po2mo: {
+			files: {
+				src: 'languages/*.po',
+				expand: true
+			}
 		}
 	} );
 	
@@ -96,6 +128,9 @@ module.exports = function( grunt ) {
 	grunt.loadNpmTasks( 'grunt-contrib-clean' );
 	grunt.loadNpmTasks( 'grunt-contrib-copy' );
 	grunt.loadNpmTasks( 'grunt-contrib-compress' );
+
+	grunt.loadNpmTasks('grunt-pot');
+	grunt.loadNpmTasks('grunt-po2mo');
 	
 	// Default task.
 	
